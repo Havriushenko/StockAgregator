0e8c3f5782c1922f5dd6b7142d6843553a87b824
Mykyta <nik.gavrushenko@gmail.com>
2020-06-08 19:59:29 +0300

    fix data base and project launch

--- src/main/java/com/test_project/havriushenko/stockAgregator/StartApplication.java
+++ src/main/java/com/test_project/havriushenko/stockAgregator/StartApplication.java
@@ -1,13 +1,17 @@
 package com.test_project.havriushenko.stockAgregator;
 
 
+import com.test_project.havriushenko.stockAgregator.service.Runner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableAutoConfiguration
+@EnableFeignClients
 public class StartApplication implements CommandLineRunner {
 
     public static void main(String... args) {
